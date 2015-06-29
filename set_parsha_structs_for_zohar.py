6fa9cb35bc6e9acea79879963cae67173a7ad2b8
stevekaplan123 <skaplan@brandeis.edu>
2015-06-29 11:01:53 -0400

    updated monday

--- set_parsha_structs_for_zohar.py
+++ set_parsha_structs_for_zohar.py
@@ -8,9 +8,18 @@ import urllib
 import urllib2
 from urllib2 import URLError, HTTPError
 
+def processDaf(daf):
+	daf_array = daf.split(':')
+	letter = daf_array[1][-1]
+	value = 1
+	if letter == 'b':
+		value = 2
+	num = int(daf_array[1][0:-1])
+	num = ((num-1)*2)+value
+	return daf_array[0]+":"+str(num)+daf_array[2]
 
 def post_index(index):
-	url = 'http://localhost:8000/api/v2/raw/index/New_Zohar'
+	url = 'http://dev.sefaria.org/api/v2/raw/index/New_Zohar'
 	indexJSON = json.dumps(index)
 	values = {
 		'json': indexJSON, 
@@ -23,8 +32,6 @@ def post_index(index):
 		print response.read()
 	except HTTPError, e:
 		print 'Error code: ', e.code
-
-#intro has he_title
 
 
 title = "New Zohar"
@@ -44,13 +51,15 @@ intro_end = Ref("New Zohar "+intro_file.readline())
 intro_ref = intro_start.to(intro_end).normal()
 intro_file.close()
 structs["nodes"].append({
-	"title":  [{
+	"titles":  [{
 				"lang": "en",
-				"text": "Introduction to the Zohar"
+				"text": "Introduction to the Zohar",
+				"primary": True
 				},
 				{
 				"lang": "he",
-				"text": "הקדמת ספר הזוהר"
+				"text": "הקדמת ספר הזוהר",
+				"primary": True
 				}],
 	"nodeType": "ArrayMapNode",
 	"refs": [],
@@ -61,53 +70,9 @@ structs["nodes"].append({
 })
 
 
-haman_file = open("Haman", 'r')
-haman_start = Ref("New Zohar "+haman_file.readline())
-haman_end = Ref("New Zohar "+haman_file.readline())
-haman_ref = haman_start.to(haman_end).normal()
-haman_file.close()
-structs["nodes"].append({
-	"title":  [{
-				"lang": "en",
-				"text": "Haman"
-				},
-				{
-				"lang": "he",
-				"text": "המן"
-				}],
-	"nodeType": "ArrayMapNode",
-	"refs": [],
-	"depth": 0,
-	"addressTypes": [],
-	"sectionNames": [],
-	"wholeRef": haman_ref
-})
-
-
-conc_file = open("Ha_Idra", 'r')
-conc_start = Ref("New Zohar "+conc_file.readline())
-conc_end = Ref("New Zohar "+conc_file.readline())
-conc_ref = conc_start.to(conc_end).normal()
-conc_file.close()
-structs["nodes"].append({
-	"title":  [{
-				"lang": "en",
-				"text": "Ha-Idra Zuta Kadisha"
-				},
-				{
-				"lang": "he",
-				"text": "האדרא זוטא קדישא"
-				}],
-	"nodeType": "ArrayMapNode",
-	"refs": [],
-	"depth": 0,
-	"addressTypes": [],
-	"sectionNames": [],
-	"wholeRef": conc_ref
-})
-
-
 for parsha in english_parshiot:
+	if parsha == "Yitro":
+		break
 	f = open(parsha, 'r')
 	start = Ref("New Zohar "+f.readline())
 	end = Ref("New Zohar "+f.readline())
@@ -124,17 +89,131 @@ for parsha in english_parshiot:
 	})
 	f.close()
 
+
+
+
+haman_file = open("Haman", 'r')
+haman_start = Ref("New Zohar "+haman_file.readline())
+haman_end = Ref("New Zohar "+haman_file.readline())
+haman_ref = haman_start.to(haman_end).normal()
+haman_file.close()
+structs["nodes"].append({
+	"titles":  [{
+				"lang": "en",
+				"text": "Haman",
+				"primary": True
+				},
+				{
+				"lang": "he",
+				"text": "המן",
+				"primary": True
+				}],
+	"nodeType": "ArrayMapNode",
+	"refs": [],
+	"depth": 0,
+	"addressTypes": [],
+	"sectionNames": [],
+	"wholeRef": haman_ref
+})
+
+dont_go = True
+for parsha in english_parshiot:
+	if parsha == "Yitro":
+		dont_go = False
+	if dont_go == True:
+		continue
+	f = open(parsha, 'r')
+	start = Ref("New Zohar "+f.readline())
+	end = Ref("New Zohar "+f.readline())
+	whole_ref = start.to(end).normal()
+		
+	structs["nodes"].append({
+		"sharedTitle": parsha,
+		"nodeType": "ArrayMapNode",
+		"refs": [],
+		"depth": 0,
+		"addressTypes": [],
+		"sectionNames": [],
+		"wholeRef": whole_ref
+	})
+	f.close()
+
+
+
+
+
+conc_file = open("Ha_Idra", 'r')
+conc_start = Ref("New Zohar "+conc_file.readline())
+conc_end = Ref("New Zohar "+conc_file.readline())
+conc_ref = conc_start.to(conc_end).normal()
+conc_file.close()
+structs["nodes"].append({
+	"titles":  [{
+				"lang": "en",
+				"text": "HaIdra Zuta Kadisha",
+				"primary": True
+				},
+				{
+				"lang": "he",
+				"text": "האדרא זוטא קדישא",
+				"primary": True
+				}],
+	"nodeType": "ArrayMapNode",
+	"refs": [],
+	"depth": 0,
+	"addressTypes": [],
+	"sectionNames": [],
+	"wholeRef": conc_ref
+})
+
+
+
+root = JaggedArrayNode()
+root.add_title("New Zohar", "en", primary=True)
+root.add_title(u"זהר הבא", "he", primary=True)
+#	root.add_title("Sefer Zohar", "en", primary=False)
+#	root.add_title("Sefer haZohar", "en", primary=False)
+#	root.add_title(u"ספר זוהר", "he", primary=False)
+#	root.add_title(u"ספר הזוהר", "he", primary=False)
+
+root.key = "new_zohar"
+root.depth = 3
+#root.lengths = [3]
+root.sectionNames = ["Volume", "Daf", "Paragraph"]
+root.addressTypes = ["Integer", "Talmud", "Integer"]
+
+root.validate()
+
+index = {
+	"title": "New Zohar",
+	"titleVariants": [""],#["Sefer Zohar", "Sefer haZohar"],
+	"sectionNames": ["Volume", "Daf", "Paragraph"],
+	"categories": ["Kabbalah"],
+	"addressTypes": ["Integer", "Talmud", "Integer"],
+	"alt_structs": {"Parasha": structs},
+	"schema": root.serialize()
+}
+
+
+#Index(index).save()
+post_index(index)
+
+
 '''
 structsJSON = json.dumps(structs)
 zohar_i = get_index("New Zohar")
 zohar_i.set_alt_structure("Parasha", structsJSON)
 zohar_JSON = json.dumps(zohar_i.schema)
 post_index(zohar_JSON)
-'''
+
+
 
 zohar_i = get_index("New Zohar")
+#zohar_i = get_index_zohar()
+#pdb.set_trace()
 obj = deserialize_tree(structs, index=zohar_i, struct_class=TitledTreeNode)
 obj.title_group = zohar_i.nodes.title_group
 obj.validate()
 zohar_i.set_alt_structure("Parasha", obj)
 zohar_i.save()
+'''
--- zohar_struct.py
+++ zohar_struct.py
@@ -11,8 +11,8 @@ from sefaria.model.schema import AddressTalmud
 def post_text(ref, text):
     textJSON = json.dumps(text)
     ref = ref.replace(" ", "_")
-    url = 'http://dev.sefaria.org/api/texts/New_Zohar'
-    values = {'json': textJSON, 'apikey': 'F4J2j3RF6fHWHLtmAtOTeZHE3MOIcsgvcgtYSwMzHtM'}
+    url = 'http://localhost:8000/api/texts/New_Zohar'
+    values = {'json': textJSON, 'apikey': 'YourApiKey'}#F4J2j3RF6fHWHLtmAtOTeZHE3MOIcsgvcgtYSwMzHtM'}
     data = urllib.urlencode(values)
     req = urllib2.Request(url, data)
     try:
@@ -23,7 +23,7 @@ def post_text(ref, text):
         print e.read()
 
 
-genesis_parshiot = ["Introduction", "Bereshit", "Noach", "Lech Lecha", "Vayera", "Chayei Sarah", "Toldot", "Vayetzei", "Vayishlach", "Vayeshev", "Miketz", "Vayigash", "Vayechi"]
+genesis_parshiot = ["Introduction", "Bereshit", "Noach", "Lech Lecha", "Vayera", "Chayei Sara", "Toldot", "Vayetzei", "Vayishlach", "Vayeshev", "Miketz", "Vayigash", "Vayechi"]
 exodus_parshiot = ["Shemot", "Vaera", "Bo", "Beshalach", "Haman", "Yitro", "Mishpatim", "Terumah", "Tetzaveh", "Ki Tisa", "Vayakhel", "Pekudei"]
 leviticus_parshiot = ["Vayikra", "Tzav", "Shmini", "Tazria", "Metzora", "Achrei Mot", "Kedoshim", "Emor", "Behar", "Bechukotai"]
 numbers_parshiot = ["Bamidbar", "Nasso", "Beha'alotcha", "Sh'lach", "Korach", "Chukat", "Balak", "Pinchas", "Matot"]
@@ -33,28 +33,28 @@ english_parshiot = genesis_parshiot+exodus_parshiot+leviticus_parshiot+numbers_p
 curr_parsha = 0
 curr_parsha_file = ""
 prev_vol = 0
-prev_daf = 0
+prev_daf = -1
 prev_para = 0
+current_line = "not header"
+prev_line = "not header"
+prev_prev_line = "not header"
 zohar_struct = range(3)
 for vol_num in range(3):
 	para_count = 1	
 	if vol_num == 0:
-		daf_count = 0
+		daf_count = -1
 		vol_file = 'intro'
 		zohar_struct[vol_num] = []
-		zohar_struct[vol_num].append([])
 	elif vol_num == 1:
-		daf_count = 2
+		daf_count = 1
 		vol_file = 'shemot_parsed'
 		zohar_struct[vol_num] = []
 		zohar_struct[vol_num].append([])
 		zohar_struct[vol_num].append([])
-		zohar_struct[vol_num].append([])
 	elif vol_num == 2:
-		daf_count = 2
+		daf_count = 1
 		vol_file = 'lnd'
 		zohar_struct[vol_num] = []
-		zohar_struct[vol_num].append([])
 		zohar_struct[vol_num].append([])
 		zohar_struct[vol_num].append([])
 	first_line = True
@@ -62,7 +62,9 @@ for vol_num in range(3):
 	for line in vol:
 		stray_tag = False
 		blank_line = False
-		if line == '\n' or (len(line)<4 and line.find('\n')>=0):
+		no_spaces = line.replace(" ", "")
+		no_return = no_spaces.replace("\n", "")
+		if len(no_return)==0:
 			blank_line = True
 		if len(line.split(' '))==1 and (line.find('<b>')>=0 or line.find('</b>')>=0):
 			stray_tag = True			
@@ -72,30 +74,49 @@ for vol_num in range(3):
 				curr_parsha_file.write('\n'+str(prev_vol+1)+":"+AddressTalmud.toStr("en", prev_daf+1)+":"+str(prev_para))
 				curr_parsha_file.close()					
 			curr_parsha_file = open(english_parshiot[curr_parsha], 'a')
-			curr_parsha_file.write(str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":1")  
+			curr_parsha_file.write(str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+2)+":1")  
 			curr_parsha += 1
 		elif blank_line==False and stray_tag==False:
+			prev_prev_line = prev_line
+			prev_line = current_line
 			new_daf = line.find('דף')
 			new_parsha = line.find('h1') #all parsha titles are surrounded by <h1> tags
 			if new_daf >= 0 and len(line.split(' ')) < 6:  
+				current_line = "daf"
 				daf_count += 1
 				zohar_struct[vol_num].append([])
-				para_count = 1		
+				prev_para = para_count
+				para_count = 1
 			elif new_parsha >= 0 and len(line.split(' ')) < 6:
-				curr_parsha_file.write('\n'+str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":"+str(para_count-1))
+				current_line = "parsha"
+				if para_count==1:
+					curr_parsha_file.write('\n'+str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count)+":"+str(prev_para-1))
+				else:
+					curr_parsha_file.write('\n'+str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":"+str(para_count-1))
 				curr_parsha_file.close()
 				curr_parsha_file = open(english_parshiot[curr_parsha], 'a')
-				curr_parsha_file.write(str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":"+str(para_count))
 				curr_parsha += 1					
 			else:
+				current_line = "neither"
 				zohar_struct[vol_num][daf_count].append(line)
-				para_count += 1		
+				para_count += 1
+			if current_line == "daf" and prev_line == "parsha": #typical case: parsha, daf
+				curr_parsha_file.write(str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":1")
+			elif current_line=="parsha" and prev_line == "daf": #Chayei Sara case: daf, parsha
+				curr_parsha_file.write(str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":1")
+			elif current_line == "neither" and prev_line == "parsha" and prev_prev_line == "neither": #Lech Lecha case: new parsha in middle of page
+				curr_parsha_file.write(str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":"+str(para_count-1))
+	
+				
 	prev_para = para_count-1
 	prev_vol = vol_num
 	prev_daf = daf_count
 	if vol_file=='lnd':
-		curr_parsha_file.write('\n'+str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":"+str(para_count))
+		curr_parsha_file.write('\n'+str(vol_num+1)+":"+AddressTalmud.toStr("en", daf_count+1)+":"+str(para_count-1))
 		curr_parsha_file.close()
+
+
+#structsJSON = json.dumps(structs)
 
 text = {
 "versionTitle": "New Zohar",
